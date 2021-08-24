9a7cae11c20ff4d185019842aac581f3c2a69d69
Victor Nikolaev Hristov <victor.h7@live.com>
2021-08-24 12:28:37 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ({headerText}) => {
+    return (
+        <header className="header-container">
+            <p className="header-container--text">{headerText}</p>
+        </header>
+    )
+}
+
+export default Header
